d41c4d0c474558e5147eb45467814a2ff4f3a335
silas1037 <silas1037@163.com>
2021-08-25 11:43:21 +0800

    ctl u2s

--- src/utfsjis.cpp
+++ src/utfsjis.cpp
@@ -21,8 +21,8 @@ int unicode_to_sjis(int u) {
 
 uint16 sjis_to_unicode(uint16 code)
 {
-	if (code >= 0xa0 && code <= 0xdf)
-		return 0xff60 + code - 0xa0;
+	//if (code >= 0xa0 && code <= 0xdf)
+	//	return 0xff60 + code - 0xa0;
 	if (code < 0x100)
 		return code;
 	return s2u[(code >> 8) - 0x80][(code & 0xff) - 0x40];
